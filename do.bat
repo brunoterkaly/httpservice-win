47dbea9bc69c94baec2fdbc367a19ef95a408d22
brunoterkaly <brunoterkaly@hotmail.com>
2016-12-30 01:10:26 +0000

    first commit

---
+++ do.bat
@@ -0,0 +1,7 @@
+echo "# httpservice-win" >> README.md
+git init
+REM git add README.md
+git add --all
+git commit -m "first commit"
+git remote add origin https://github.com/brunoterkaly/httpservice-win.git
+git push -u origin master
